1b73cbabb42f95667c5122fcf225c3ae03cde28f
Renz <galang.renzalbert@gmail.com>
2022-10-30 21:24:04 +0800

    fix: document factory

--- app/Domain/Documents/Models/Document.php
+++ app/Domain/Documents/Models/Document.php
@@ -3,17 +3,26 @@
 namespace App\Domain\Documents\Models;
 
 use App\Domain\Users\Models\User;
+use Database\Factories\DocumentFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Plank\Mediable\Mediable;
 
 class Document extends Model
 {
-    use Mediable;
+    use HasFactory,
+        Mediable;
 
     protected $fillable = [
         'name',
         'user_id'
     ];
+
+    protected static function newFactory()
+    {
+        return DocumentFactory::new();
+    }
 
     public function attachments()
     {
@@ -23,5 +32,12 @@ class Document extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeSearch(Builder $query, string $keyWord)
+    {
+        return $query->orWhere('name', 'like', "%$keyWord")
+            ->orWhere('name', 'like', "$keyWord%")
+            ->orWhere('name', 'like', "%$keyWord%");
     }
 }
